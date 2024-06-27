c4a78edc1cf33b27481f41a18be6d4c3bdf5d505
kanishka <kanishka@Kanishka.kanishka>
2024-06-27 18:18:38 +0530

    the files has pushed

---
+++ eventjs/class.js
@@ -0,0 +1,18 @@
+function test(user, password)
+{
+this.user = user;
+this.password = password;
+}
+
+
+// }
+// function test2(user,password)
+// {
+// this.user = user;
+// this.password = password;
+// }
+function Result(user,password){
+console.log(super(user))
+console.log(super(password))
+
+}
